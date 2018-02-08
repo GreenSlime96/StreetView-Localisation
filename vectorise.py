7bcae760e4206250e8a773564cad2165b4a4d53b
Kheng Boon Pek <boonpek96@gmail.com>
2018-02-08 16:58:44 +0000

    removed dead code, changed extractor to sift

--- vectorise.py
+++ vectorise.py
@@ -18,7 +18,7 @@ def build_features(images, filename, bins=16):
         sys.exit('filename in use: {}'.format(filename))
 
     # hist = np.empty(3 * bins * len(images), dtype=np.int32)
-    extractor = cv2.xfeatures2d.SIFT_create()
+    sift = cv2.xfeatures2d.SIFT_create()
 
     desc = []
     targ = []
@@ -26,13 +26,14 @@ def build_features(images, filename, bins=16):
     idx = 0
     for i in range(len(images)):
         image = cv2.imread(images[i])
-        kp, des = extractor.detectAndCompute(image, None)
+        kp, des = sift.detectAndCompute(image, None)
 
         # for c in range(3):
         #     hist = cv2.calcHist([image], [c], None, [bins], [0, 256])[:,0]
         #     hists[idx:idx+bins] = hist
         #     idx += bins
 
+        # TODO: update this to use a more compact representation
         targ.extend([i] * len(des))
         desc.extend(des.astype(np.uint8))
 
@@ -66,46 +67,6 @@ def main():
     # Use a SIFT feature extractor
     build_features(images, args.outfile)
 
-
-
-"""
-TODO: prune function
-polish pipeline
-save features?
-"""
-
-
-# FLANN_INDEX_KDTREE = 1
-# index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-# search_params = dict(checks=50)   # or pass empty dictionary
-
-# flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-# # Build random samples
-# # indices = random.sample(range(len(descriptors)), 1000)
-# # des = np.asarray([descriptors[i] for i in indices])
-
-# # matches = flann.knnMatch(des, descriptors, k=2)
-
-# # for i in range(len(matches)):
-# #     match = matches[i]
-# #     if match[0].trainIdx != indices[i]:
-# #         print('false')
-
-# image = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
-# kp, des = sift.detectAndCompute(image, None)
-
-# matches = flann.knnMatch(des, descriptors, k=5)
-# votes = []
-
-# for m in matches:
-#     for k in m:
-#         votes.append(target[k.trainIdx])
-
-# c = Counter(votes)
-
-# for i in c.most_common(5):
-#     print(files[i])
 
 if __name__ == "__main__":
     main()
