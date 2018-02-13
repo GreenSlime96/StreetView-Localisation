f466d87f9eab81139fb4bd7704020ae48ab49ede
Kheng Boon Pek <boonpek96@gmail.com>
2018-02-13 01:42:56 +0000

    almost there

--- search.py
+++ search.py
@@ -1,4 +1,5 @@
 import glob
+import math
 
 from collections import Counter
 from os import path
@@ -8,6 +9,7 @@ import nmslib
 import numpy as np
 
 from geopy.distance import distance
+from scipy.stats import norm
 
 from util import DataLoader
 
@@ -24,7 +26,6 @@ from util import DataLoader
 # 3.84 ms ± 172 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
 
 
-
 class Search(object):
     def __init__(self, dataset, index, k=5):
         self.dataset = dataset
@@ -36,16 +37,20 @@ class Search(object):
         results = self.index.knnQueryBatch(queries, self.k + 1)
         i, d = map(np.array, zip(*results))
 
-        # baseline, before pruning
-        self.i = i
-
-        # compute mask according to eqn. 1
+        # compute pruning mask to filter features
         mask = d[:, 0] / d[:, -1] <= 0.64
 
         # prune output, and remove last values from query
         self.queries = queries[mask]
         self.indices = i[mask, :-1]
         self.distances = d[mask, :-1]
+
+        # store these values which are frequently accessed
+        self.imgidx = self.dataset.target2index(self.indices)
+        self.coords = (self.imgidx / 5).astype(np.uint32)
+
+        # reset for re-memoisation
+        self.smoothed = None
 
     def node_cost(self, i, m):
         return self.distances[i, m]
@@ -58,7 +63,59 @@ class Search(object):
 
         return distance(c1, c2).meters
 
+    def coords(self):
+        return [self.dataset.coordinates[x] for x in self.coords.flatten()]
+
     def search(self):
+        counts = np.bincount(self.coords.flatten()).astype(np.uint16)
+        indices = np.argmax(counts)
+
+        return (indices, counts)
+
+    def confidence(self):
+        smoothed = self.smooth()
+
+        expected = np.argmax(smoothed)
+        mindist = self.dataset.mindist[expected] ** 4
+
+        # TODO: need to figure out how this actually works
+        confidence = 0
+        for i, c in enumerate(smoothed):
+            confidence += self.dataset.distances[expected][i] ** 4 * c
+
+        return -3 + confidence / mindist
+
+    def count(self):
+        pass
+
+    def smooth(self):
+        if self.smoothed is not None:
+            return self.smoothed
+
+        bins = np.bincount(self.coords.flatten())
+        smoothed = np.zeros_like(bins, dtype=float)
+
+        for i, c in enumerate(bins):
+            mindistsq = 2 * self.dataset.mindist[i] ** 2
+
+            for j, d in enumerate(bins):
+                distsq = self.dataset.distances[i][j] ** 2
+                smoothed[i] += math.exp(-distsq / mindistsq) * d
+
+        self.smoothed = smoothed
+        return smoothed
+
+
+class VideoProcessor(object):
+    def __init__(self, fps, skip=6):
+        self.sift = cv2.xfeatures2d.SIFT_create()
+        self.started = False
+        self.fps = fps
+
+    def process(self, frame):
+        pass
+
+    def finalise(self):
         pass
 
 
@@ -69,28 +126,119 @@ def main():
     dataset = DataLoader.create('data')
     search = Search(dataset, index)
 
-    images = glob.glob('data/test_set/*.jpg')
+    # images = glob.glob('data/test_set/*.jpg')
+    cap = cv2.VideoCapture('data/test_videos/6.mp4')
     sift = cv2.xfeatures2d.SIFT_create()
 
-    for image in images:
-        print("working on: {}".format(image))
+    lastcoord = None
+    velocity = None
 
-        img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-        kp, des = sift.detectAndCompute(img, None)
+    frames = 0
+    coords = []
+    fps = int(cap.get(cv2.CAP_PROP_FPS) + 0.5)
+    print("Input FPS: {}".format(fps))
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        kp, des = sift.detectAndCompute(gray, None)
 
         search.update(des)
 
-        filename = path.splitext(image)[0]
+        coords.extend([dataset.target2coord(x)
+                       for x in search.indices.flatten()])
 
-        with open(filename + '_base', 'w') as fp:
-            coords = [dataset.target2coord(x) for x in search.i[:,:-1].flatten()]
-            for c in coords:
-                fp.write("          new google.maps.LatLng({}, {}),\n".format(c[0], c[1]))
+        if frames == fps:
+            frames = 0
+            coord = None
 
-        with open(filename + '_prune', 'w') as fp:
-            coords = [dataset.target2coord(x) for x in search.indices[:,:-1].flatten()]
-            for c in coords:
-                fp.write("          new google.maps.LatLng({}, {}),\n".format(c[0], c[1]))
+            # compute probability of top-10 votes
+            common = Counter(coords).most_common(50)
+            coords = []
+
+            # total = sum([n for c, n in common])
+            # common = [(c, n / total) for c, n in common]
+
+            maxprob = 0
+            maxcoord = None
+            maxdist = None
+
+            if lastcoord and velocity:
+                newcoord = (lastcoord[0] + velocity[0],
+                            lastcoord[1] + velocity[1])
+
+                # d_v = distance(newcoord, lastcoord).meters
+                # d_v = d_v if d_v else 12
+
+                for c, n in common:
+                    dist = distance(newcoord, c).meters
+
+                    prob = norm.pdf(dist / 50) * n
+
+                    if prob > maxprob:
+                        maxdist = dist
+                        maxprob = prob
+                        maxcoord = c
+
+                # if maxdist > 100:
+                #     maxcoord = lastcoord
+
+            else:
+                # topmost item
+                maxcoord = common[0][0]
+                lastcoord = maxcoord
+
+            coord = maxcoord
+
+            if coord != lastcoord or not velocity:
+                velocity = (coord[0] - lastcoord[0], coord[1] - lastcoord[1])
+                print("{{lat:{},lng:{}}},".format(coord[0], coord[1]))
+
+            lastcoord = coord
+
+        frames += 1
+
+    cap.release()
+
+    # for image in images:
+    #     print("working on: {}".format(image))
+
+    #     img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    #     kp, des = sift.detectAndCompute(img, None)
+
+    #     search.update(des)
+
+    #     filename = path.splitext(image)[0]
+
+    # with open(filename + '_base', 'w') as fp:
+    #     coords = [dataset.target2coord(x)
+    #               for x in search.i[:, :-1].flatten()]
+    #     for c in coords:
+    #         fp.write(
+    #             "new google.maps.LatLng({}, {}),\n".format(c[0], c[1]))
+
+    # with open(filename + '_prune', 'w') as fp:
+    #     coords = [dataset.target2coord(x)
+    #               for x in search.indices[:, :-1].flatten()]
+    #     for c in coords:
+    #         fp.write(
+    #             "new google.maps.LatLng({}, {}),\n".format(c[0], c[1]))
+
+    # with open(filename + '_smooth', 'w') as fp:
+    #     coords = []
+    #     for i, c in enumerate(search.smooth()):
+    #         coords.extend([dataset.coordinates[i]] * int(c))
+
+    #     print("{}: ".format(search.confidence()))
+
+    #     for c in coords:
+    #         fp.write(
+    #             "new google.maps.LatLng({}, {}),\n".format(c[0], c[1]))
+
 
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import glob
+import math
 import sys
 
 from os import path
@@ -11,13 +12,13 @@ SEPARATOR_MULTIPLIER = 100
 # In [28]: R = 6378137
 #     ...: x0 = y0 = None
 #     ...: for i in coordinates:
-#     ...:     lat, lon = map(lambda x: math.radians(float(x)), i.split(','))
-#     ...:     x = R * math.cos(lat) * math.cos(lon)
-#     ...:     y = R * math.cos(lat) * math.sin(lon)
-#     ...:     if not x0 and not y0:
-#     ...:         x0 = x
-#     ...:         y0 = y
-#     ...:     print("{}\t{}".format(x0 - x, y0 - y))
+#lat, lon = map(lambda x: math.radians(float(x)), i.split(','))
+#x = R * math.cos(lat) * math.cos(lon)
+#y = R * math.cos(lat) * math.sin(lon)
+# if not x0 and not y0:
+#    x0 = x
+#    y0 = y
+#print("{}\t{}".format(x0 - x, y0 - y))
 #     ...:
 
 
@@ -28,14 +29,31 @@ class DataLoader(object):
         self.targets = np.load(
             path.join(folder, 'features_targ'), mmap_mode='r')
 
-    def target2coord(self, index):
-        index = np.searchsorted(self.targets, index, side='right')
-        image = self.images[index]
+        self.distances = np.zeros((len(self.coordinates), ) * 2)
+        self.mindist = np.full(len(self.coordinates), np.inf)
 
-        filename = path.basename(image)
+        for i, c in enumerate(self.coordinates):
+            for j, d in enumerate(self.coordinates[i + 1:], i + 1):
+                dist = fast_distance(c, d)
+                self.distances[i][j] = self.distances[j][i] = dist
+
+                self.mindist[i] = min(self.mindist[i], dist)
+                self.mindist[j] = min(self.mindist[j], dist)
+
+    def target2index(self, index):
+        return np.searchsorted(self.targets, index, side='right')
+
+    def index2coord(self, index):
+        img = self.images[index]
+        filename = path.basename(img)
         index = path.splitext(filename)[0].split('_')[0]
 
         return self.coordinates[int(index)]
+
+    def target2coord(self, index):
+        index = self.target2index(index)
+
+        return self.index2coord(index)
 
     @classmethod
     def create(cls, folder='data'):
@@ -70,3 +88,14 @@ def load_images(directory):
         return sorted(files, key=name_hash)
     else:
         sys.exit('not a directory: {}'.format(directory))
+
+
+def fast_distance(c1, c2):
+    la1, lo1 = math.radians(c1[0]), math.radians(c1[1])
+    la2, lo2 = math.radians(c2[0]), math.radians(c2[1])
+    dlo = lo2 - lo1
+    dla = la2 - la1
+    a = math.sin(dla / 2)**2 + math.cos(la1) * \
+        math.cos(la2) * math.sin(dlo / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return 6371008 * c
